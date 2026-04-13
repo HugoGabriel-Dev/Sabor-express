13c53af9dc7b6543bb2da84c56eecf76da9e2775
HugoGabriel <hg116518@gmail.com>
2026-04-13 17:31:05 -0300

    inicializando funcoes especificas do projeto

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-print('hello, world!')
+import os
+
+def exibir_nome_app():
+    print('''
+
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+    ''')
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
+def finalizar_app():
+    os.system('cls')
+    print('finalizando programa!!!')  
+def avaliar_opcao_escolhida():
+    opcao_escolhida = int(input('Escolha uma Opção: '))
+    if opcao_escolhida == 1:
+        print('Cadastrar seu restaurante')
+    elif opcao_escolhida == 2:
+        print('lista restaurantes')
+    elif opcao_escolhida == 3:
+        print('ativar restaurante')
+    else:
+        finalizar_app()
+def  main():
+    exibir_nome_app()
+    exibir_opcoes()  
+    avaliar_opcao_escolhida()
+if __name__ == '__main__':
+    main()
